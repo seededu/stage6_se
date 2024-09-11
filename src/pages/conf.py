3db3a93bd8ae8e99a325f7358ecd0d110cd53f9b
alipwong <a.wong@sydney.edu.au>
2024-09-11 16:59:45 +1000

    updated requirements.txt with the sphinx-design package

--- src/pages/conf.py
+++ src/pages/conf.py
@@ -14,7 +14,7 @@ author = 'Alison Wong, Stephen Tierney'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_exec_code', 'sphinx_togglebutton', 'sphinx_design'
+    'sphinx_exec_code', 'sphinx_design'
 ]
 
 templates_path = ['_templates']
